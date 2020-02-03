53489bf7dc5d65d625947be6bd8f4b10b60455cb
kaitosuganoo0807 <k.0807roadster@gmail.com>
2020-02-03 16:40:15 +0900

    update

--- index.php
+++ index.php
@@ -1,20 +1,56 @@
+<?php 
+$name = "";
+$message = "";
+
+if (isset($_POST["send"]) === true) {
+  $name = $_POST["name"];
+  $message = $_POST["message"];
+  $fp = fopen("bord.txt", "a");
+  fwrite($fp, $name, "\t" . $message . "\n");
+  fclose($fp);
+}
+
+$fp = fopen("bord.txt", "r");
+
+$bord_array = [];
+while ($line = fgets($fp)) {
+  $temp = explode("\t", $line);
+  $temp_array = [
+    "name" => $temp[0],
+    "message" => $temp[1]
+  ];
+  $bord_array[] = $temp_array;
+}
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="ja">
 <head>
   <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>cuisine-php</title>
+  <title>PHPで掲示板作ってみた</title>
   <link rel="stylesheet" type="text/css" href="stylesheet.css">
 </head>
 <body>
-  <div class="menu-wrapper container">
-    <h1 class="logo">cafe</h1>
-    <h3>メニュー</h3>
-    <form method="post" action="">
-      <div class="menu-items">
-      
-      </div>
-    </form>
-  </div>
+  <form action="" method="post">
+    <div>
+      <label for="name">名前</label>
+      <input type="text" id="name" name="name">
+    </div>
+    <div>
+      <label for="message">内容</label>
+      <input type="text" id="message" name="message">
+    </div>
+    <input type="submit" name="send" value="送信する">
+  </form>
+  <h2>表示欄</h2>
+  <ul>
+    <?php foreach ($bord_array as $data): ?>
+      <?= "<li>" ?>
+      <?= $data["name"] . ":" . $data["message"]; ?>
+      <?= "</li>" ?>
+    <?php endforeach ?>
+  </ul>
 </body>
 </html>
