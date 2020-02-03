a8e3c3edcabfeeade0d90cb11f69c3c16a72b949
kaitosuganoo0807 <k.0807roadster@gmail.com>
2020-02-03 15:59:10 +0900

    initialize

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>cuisine-php</title>
+  <link rel="stylesheet" type="text/css" href="stylesheet.css">
+</head>
+<body>
+  <div class="menu-wrapper container">
+    <h1 class="logo">cafe</h1>
+    <h3>メニュー</h3>
+    <form method="post" action="">
+      <div class="menu-items">
+      
+      </div>
+    </form>
+  </div>
+</body>
+</html>
